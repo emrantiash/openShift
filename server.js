5e099e07f7412450d1de6db645ce346329c63073
emrantiash <tiash03@yahoo.com>
2018-05-27 12:42:52 +0600

    update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var db = require('./server/database/database.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+   ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0' ;
 <!-- controllers -->
 var showcontroller = require('./server/controllers/showController');
 
@@ -29,8 +32,3 @@ app.post('/adduser',showcontroller.addUser);
 app.delete('/deleteuser/:id',showcontroller.deleteUser);
 //app.post('/login',usercontroller.logIn);
 
-db.sync().then(function(){
-	app.listen(3000,function(){
-		console.log("it works..");
-	})
-})
