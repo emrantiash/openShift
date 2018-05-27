c92d7a3190bee668f512fe6edf7a5e0aae685b89
emrantiash <tiash03@yahoo.com>
2018-05-27 12:44:57 +0600

    update

--- server.js
+++ server.js
@@ -1,25 +1,11 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var db = require('./server/database/database.js');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+var express = require('express'),
+    app     = express(),
+   
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-   ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0' ;
-<!-- controllers -->
-var showcontroller = require('./server/controllers/showController');
-
-<!-- allow to hook up -->
-	app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "DELETE");
-  next();
-});
-
-<!-- end -->
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     
 app.get('/', function (req, res) {
     
@@ -27,8 +13,7 @@ app.get('/', function (req, res) {
     
 });
 
-app.get('/showuser',showcontroller.allUser);
-app.post('/adduser',showcontroller.addUser);
-app.delete('/deleteuser/:id',showcontroller.deleteUser);
-//app.post('/login',usercontroller.logIn);
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
 
+module.exports = app ;
